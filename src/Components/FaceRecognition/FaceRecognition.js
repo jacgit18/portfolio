482fe6b18bb79a004fef32677f3ac701a280498a
jacgit18 <jacdevgoo2020@gmail.com>
2021-07-08 18:23:27 -0400

    finshed frontend prototype and need to fix particles

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './FaceRecognition.css';
 
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl, box }) => {
 
     return (
-        <div style={{ }}>
-            <p> FaceRecognition</p>
-        </div>
-        
+        <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+      </div>
+    </div>
         );
 
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 // import Particles from 'react-particles-js';
+
 import Clarifai from 'clarifai';
+import Register from '../Components/Register/Register';
+import Signin from '../Components/Signin/Signin';
 
 import Navigation from "../Components/Navigation/Navigation";
 import Logo from "../Components/Logo/Logo";
 import ImageLinkForm from "../Components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "../Components/FaceRecognition/FaceRecognition";
 import Rank from "../Components/Rank/Rank";
-
 import Banner from "../Components/Banner";
 // import Body from "../Components/Body";
-
 import Footer from "../Components/Footer";
-
-import '../style/App.css';
 // import Scroll from "../Components/Scroll";
 // import ErrorBoundary from "../Components/ErrorBoundary";
+import '../style/App.css';
+
 
 const app = new Clarifai.App({
  apiKey: 'd39499b3505a4a00b787eb91e10ce51f'
@@ -49,93 +50,91 @@ class App extends Component {
         joined: ''
       }
     }
+
   }
 
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then((response) => response.json())
-  //   .then((users)=>{
-  //       this.setState({robots:users})
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
 
-  //   })
 
-  // }
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
 
   displayFaceBox = (box) => {
     this.setState({box: box});
   }
 
   onInputChange = (event) => {
-    // this.setState({input: event.target.value});
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
+    // console.log(event.target.value);
   }
+
+
 
   onButtonSubmit = () => {
-    // this.setState({imageUrl: this.state.input});
-    console.log('click');
-    app.models.predict("a403429f2ddf4b49b307e318f00e528b","https://samples.clarifai.com/face-det.jpg")
-    .then(
-      function (response) {
-      console.log(response)
-    }, 
-    function (err) {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+      // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+      // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+      // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+      // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+      // so you would change from:
+      // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      // to:
+      // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(response => {
+      console.log('hi', response)
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}))
+          })
 
-    }
-    )
-
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
+}
+onRouteChange = (route) => {
+  if (route === 'signout') {
+    this.setState({isSignedIn: false})
+  } else if (route === 'home') {
+    this.setState({isSignedIn: true})
   }
+  this.setState({route: route});
+}
 
-
-//   onButtonSubmit = () => {
-//     this.setState({imageUrl: this.state.input});
-//     app.models
-//     .predict(
-//       // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-//       // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-//       // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-//       // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-//       // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
-//       // so you would change from:
-//       // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-//       // to:
-//       // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
-//       Clarifai.FACE_DETECT_MODEL,
-//       this.state.input)
-//     .then(response => {
-//       console.log('hi', response)
-//       if (response) {
-//         fetch('http://localhost:3000/image', {
-//           method: 'put',
-//           headers: {'Content-Type': 'application/json'},
-//           body: JSON.stringify({
-//             id: this.state.user.id
-//           })
-//         })
-//           .then(response => response.json())
-//           .then(count => {
-//             this.setState(Object.assign(this.state.user, { entries: count}))
-//           })
-
-//       }
-//       this.displayFaceBox(this.calculateFaceLocation(response))
-//     })
-//     .catch(err => console.log(err));
-// }
 
   render() {
-    // const { robots, searchfield } = this.state;
-    // const filteredRobots = robots.filter( (robot) => {
-    //     return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-
-    // }
-    // )
-
-    // if(!robots.length) {
-    //     return <h1> Loading</h1>
-    // } else{
-
-   
-    // );
+    const { isSignedIn, imageUrl, route, box } = this.state;
 
     return (
       <div className="App ">
@@ -151,9 +150,11 @@ class App extends Component {
         {/* <ErrorBoundary>
         </ErrorBoundary> */}
 
-        <Navigation />
-        <Logo />
-        <Rank
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+          ? <div>
+              <Logo />
+              <Rank
                 name={this.state.user.name}
                 entries={this.state.user.entries}
               />
@@ -161,22 +162,22 @@ class App extends Component {
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit}
               />
-                            <FaceRecognition  />
-
-              {/* <FaceRecognition box={box} imageUrl={imageUrl} /> */}
-
-
-        {/* <Body /> */}
-        {/* </Scroll> */}
-
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
+          : (
+             route === 'signin'
+             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            )
+        }
         <Footer />
 
       </div>
+      
 
     )}
 
 
-  // }
 }
 
 export default App;
