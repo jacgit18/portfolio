1c8658b2c55fb922effa3bb04423820b5e69ed15
jacgit18 <jacdevgoo2020@gmail.com>
2021-07-23 21:51:05 -0400

    started docker setup

---
+++ backend/controller/dev.js
@@ -0,0 +1,6 @@
+exports.version = (req, res, next) => {
+    return res.status(200).json('hello world')
+}
+
+
+
---
+++ backend/routes/dev.js
@@ -0,0 +1,11 @@
+const controller = require('../controller/dev');
+const router = require('express').Router();
+
+router.get('/version', controller.version);
+
+
+// router.post('/register', controller.handleRegister);
+
+// app.post("/register", (req, res) =>{ register.handleRegister(req, res, db, bcrypt)});
+
+module.exports = router;
